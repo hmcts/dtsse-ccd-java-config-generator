818801c780403c486201a7982cb0632b8fd9d113
taleb <tech@benouaer.com>
2023-05-10 13:45:45 +0100

    DTSSE-3009: add java validation

---
+++ validation/test/java/uk/gov/hmcts/TestConfiguration.java
@@ -0,0 +1,8 @@
+package uk.gov.hmcts;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// For bootstrapping the spring context in integration tests.
+@SpringBootApplication
+public class TestConfiguration {
+}
